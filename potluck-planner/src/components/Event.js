ebe97c89762d6987c1893329f581cb78290fffae
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-16 11:30:31 -0700

    added event cards, click to expand button, info

--- potluck-planner/src/components/Event.js
+++ potluck-planner/src/components/Event.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Event = props => {
     const {event} = props
+    const [showInfo, setShowInfo] = useState(false)
+
+    const toggleInfo = () => {
+        setShowInfo(!showInfo)
+    }
 
     return(
+        <div className='event-card'>
         <div>
-            <h3>Title: {event.title}</h3>
-            <p>Date: {event.date}</p>
-            <p>Time: {event.time}</p>
+            <span>Title: {event.title}</span>
+            <span>Date: {event.date}</span>
+            <span>Time: {event.time}</span>
+            <button onClick={toggleInfo}>Click to Expand</button>
+        </div>
+        <div className={!showInfo ? 'event-info' : null }>
             <p>Location: {event.location}</p>
             <p>Description: {event.description}</p>
-            <p>Guests: {event.guests}</p>
+            <p>Guests: {event.guests}</p>    
+        </div>
+
         </div>
     )
 }
