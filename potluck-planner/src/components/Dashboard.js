e963446b5e0da6f98a1f955911478141c123ccdf
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-11-16 10:20:43 -0700

    npm installs, folders

---
+++ potluck-planner/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+const Dashboard = props => {
+    return (
+        <div>
+        Hello
+        </div>
+    )
+}
+
+export default Dashboard
